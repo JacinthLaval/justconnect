c46fc538dfaf40aa8530ee536c0def6c3b2329df
JacinthLaval <47199523+JacinthLaval@users.noreply.github.com>
2023-05-19 11:54:35 -0500

    Update justconnect.py

--- justconnect.py
+++ justconnect.py
@@ -1,1 +1,7 @@
+# Establish Snowflake session
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
 
+session = create_session()
+st.success("Connected to Snowflake!")
